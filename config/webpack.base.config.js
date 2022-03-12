c02a4de92f4e129c9a31d03880496aa906cb2164
cqhaisenbao <fanyu9353@gmail.com>
2022-03-13 00:39:15 +0800

    ci: ts-loader 配置

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -4,24 +4,28 @@ const webpackBaseConfig = {
     entry: path.join(__dirname, '../src/index.jsx'),
     output: {
         path: path.join(__dirname, '../dist'),
-        filename: '[name].[fullhase:4].js'
+        filename: '[name].[fullhase:4].js',
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     module: {
         rules: [
             {
                 test: /\.js[x]/,
                 use: 'babel-loader',
-                exclude: /node_modules/
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.ts[x]/,
+                use: 'ts-loader',
             },
             {
                 test: /\.(sc|c)ss/,
-                use: ['style-loader', 'css-loader']
+                use: ['style-loader', 'css-loader'],
             },
-        ]
-    }
-}
+        ],
+    },
+};
 
 module.exports = webpackBaseConfig;

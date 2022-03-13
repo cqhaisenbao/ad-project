966805fe423da24cad679c8603cf33e63ea52ae0
cqhaisenbao <fanyu9353@gmail.com>
2022-03-13 11:53:50 +0800

    ci: scss模组化配置

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,12 @@
 import { useLocation } from 'react-router';
-import './style.scss';
+import styles from './style.module.scss';
 
 const IndexPage = () => {
     // 获取路由信息
     const router = useLocation();
     console.log(router);
     return (
-        <div className="wrapper">
+        <div className={styles.wrapper}>
             <h1>Hello indexPage</h1>
         </div>
     );

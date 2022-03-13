497dd4ceea14ea075935afd16f0a51ee74099498
cqhaisenbao <fanyu9353@gmail.com>
2022-03-13 22:19:38 +0800

    ci: utils alias配置

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -13,17 +13,18 @@ const webpackBaseConfig = {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
         alias: {
             pages: path.join(__dirname, '../src/pages'),
+            '@utils': path.join(__dirname, '../src/utils'),
         },
     },
     module: {
         rules: [
             {
-                test: /\.js[x]/,
+                test: /\.jsx?$/,
                 use: 'babel-loader',
                 exclude: /node_modules/,
             },
             {
-                test: /\.ts[x]/,
+                test: /\.tsx?$/,
                 use: 'ts-loader',
             },
             {
